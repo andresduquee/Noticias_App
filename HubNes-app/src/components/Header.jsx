3aaf258ea8129e8b252c1a89b318109b537a2cb7
andresduquee <andresduque.e@gmail.com>
2025-11-17 11:31:45 -0500

    Refactor Header component for improved readability

--- HubNes-app/src/components/Header.jsx
+++ HubNes-app/src/components/Header.jsx
@@ -1,16 +1,15 @@
 import { Icon } from "@iconify/react";
-
 export const Header = () => {
-  return (
-    <header className="bg-dark sticky-top shadow-sm w-100">
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark w-100">
-        <div className="container-fluid">
-          <a className="navbar-brand fw-bold fs-4" href="#">
-            <span className="badge bg-primary rounded p-2 me-2">N</span>
-            NewsHub
-          </a>
-        </div>
-      </nav>
-    </header>
-  );
-};
+    return (
+        <header className="bg-dark sticky-top shadow-sm w-100 ">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <div className="container-fluid">
+                    <a className="navbar-brand fw-bold fs-4" href="#">
+                        <span className="badge bg-primary rounded p-2 me-2">N</span>
+                        NewsHub
+                    </a>
+                </div>
+            </nav>
+        </header>
+    )
+}
