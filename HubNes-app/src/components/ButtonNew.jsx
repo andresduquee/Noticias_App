bfd4cf09190581b157a96c65681104b1005d586f
IvanMarin <ji.marin04@ciaf.edu.co>
2025-11-17 11:59:55 -0500

    cambie el componente boton

--- HubNes-app/src/components/ButtonNew.jsx
+++ HubNes-app/src/components/ButtonNew.jsx
@@ -3,7 +3,7 @@ import { Icon } from "@iconify/react";
 export const ButtonNew = (linkNoticia) => {
     return (
         <a href={linkNoticia}><button type="button" className="btn btn-warning btn-floating" data-mdb-ripple-init>
-            Ver noticia
+            Ir a la Noticia
             <Icon icon="mingcute:external-link-line" width="24" height="24" />
         </button></a>
         
